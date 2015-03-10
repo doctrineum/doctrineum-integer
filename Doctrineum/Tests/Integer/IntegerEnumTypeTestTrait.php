4f10d0579789a3e58bc4eee4c8d4791d783c04e9
Jaroslav Tyc <mail@jaroslavtyc.com>
2015-03-10 09:09:57 +0100

    Enum type has tested subtype

--- Doctrineum/Tests/Integer/IntegerEnumTypeTestTrait.php
+++ Doctrineum/Tests/Integer/IntegerEnumTypeTestTrait.php
@@ -5,6 +5,7 @@ use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\DBAL\Types\Type;
 use Doctrineum\Integer\IntegerEnumType;
 use Doctrineum\Scalar\EnumInterface;
+use Doctrineum\Scalar\EnumType;
 
 trait IntegerEnumTypeTestTrait
 {
@@ -27,9 +28,7 @@ trait IntegerEnumTypeTestTrait
     protected function setUp()
     {
         $enumTypeClass = $this->getEnumTypeClass();
-        if (Type::hasType($enumTypeClass::getTypeName())) {
-            Type::overrideType($enumTypeClass::getTypeName(), $enumTypeClass);
-        } else {
+        if (!Type::hasType($enumTypeClass::getTypeName())) {
             Type::addType($enumTypeClass::getTypeName(), $enumTypeClass);
         }
     }
@@ -37,6 +36,14 @@ trait IntegerEnumTypeTestTrait
     protected function tearDown()
     {
         \Mockery::close();
+
+        $enumTypeClass = $this->getEnumTypeClass();
+        $enumType = Type::getType($enumTypeClass::getTypeName(), $enumTypeClass);
+        /** @var EnumType $enumType */
+        if ($enumType::hasSubtype(TestSubtype::class)) {
+            /** @var \PHPUnit_Framework_TestCase $this */
+            $this->assertTrue($enumType::removeSubtype(TestSubtype::class));
+        }
     }
 
     /**
@@ -60,12 +67,14 @@ trait IntegerEnumTypeTestTrait
     }
 
     /** @test */
-    public function can_create_enum_type_instance()
+    public function instance_can_be_obtained()
     {
         $enumTypeClass = $this->getEnumTypeClass();
         $instance = $enumTypeClass::getType($enumTypeClass::getTypeName());
         /** @var \PHPUnit_Framework_TestCase $this */
         $this->assertInstanceOf($enumTypeClass, $instance);
+        
+        return $instance;
     }
 
     /** @test */
@@ -101,6 +110,10 @@ trait IntegerEnumTypeTestTrait
         /** @var \PHPUnit_Framework_TestCase|IntegerEnumTypeTestTrait $this */
         $this->assertSame($value, $enumType->convertToDatabaseValue($enum, $this->getAbstractPlatform()));
     }
+
+    /**
+     * conversions to PHP value
+     */
 
     /**
      * @test
@@ -242,4 +255,162 @@ trait IntegerEnumTypeTestTrait
         }, $this->getAbstractPlatform());
     }
 
+    /**
+     * subtype tests
+     */
+
+    /**
+     * @param EnumType $enumType
+     * @return IntegerEnumType
+     *
+     * @test
+     * @depends instance_can_be_obtained
+     */
+    public function can_register_subtype(EnumType $enumType)
+    {
+        /** @var \PHPUnit_Framework_TestCase|IntegerEnumTypeTestTrait $this */
+        $this->assertTrue($enumType::addSubtype(TestSubtype::class, '~foo~'));
+        $this->assertTrue($enumType::hasSubtype(TestSubtype::class));
+
+        return $enumType;
+    }
+
+    /**
+     * @param EnumType $enumType
+     *
+     * @test
+     * @depends can_register_subtype
+     */
+    public function can_unregister_subtype(EnumType $enumType)
+    {
+        /**
+         * @var \PHPUnit_Framework_TestCase|IntegerEnumTypeTestTrait $this
+         *
+         * The subtype is unregistered because of tearDown clean up
+         * @see IntegerEnumTypeTestTrait::tearDown
+         */
+        $this->assertFalse($enumType::hasSubtype(TestSubtype::class), 'Subtype should not be registered yet');
+        $this->assertTrue($enumType::addSubtype(TestSubtype::class, '~foo~'));
+        $this->assertTrue($enumType::removeSubtype(TestSubtype::class));
+        $this->assertFalse($enumType::hasSubtype(TestSubtype::class));
+    }
+
+    /**
+     * @param EnumType $enumType
+     *
+     * @test
+     * @depends can_register_subtype
+     */
+    public function subtype_returns_proper_enum(EnumType $enumType)
+    {
+        /**
+         * @var \PHPUnit_Framework_TestCase|IntegerEnumTypeTestTrait $this
+         */
+        $this->assertTrue($enumType::addSubtype(TestSubtype::class, $regexp = '~456~'));
+        /** @var AbstractPlatform $abstractPlatform */
+        $abstractPlatform = \Mockery::mock(AbstractPlatform::class);
+        $matchingValueToConvert = 123456789;
+        $this->assertRegExp($regexp, "$matchingValueToConvert");
+        /**
+         * Used TestSubtype returns as an "enum" the given value, which is $valueToConvert in this case,
+         * @see \Doctrineum\Tests\Scalar\TestSubtype::getEnum
+         */
+        $this->assertSame($matchingValueToConvert, $enumType->convertToPHPValue($matchingValueToConvert, $abstractPlatform));
+    }
+
+    /**
+     * @param EnumType $enumType
+     *
+     * @test
+     * @depends can_register_subtype
+     */
+    public function default_enum_is_given_if_subtype_does_not_match(EnumType $enumType)
+    {
+        /**
+         * @var \PHPUnit_Framework_TestCase|IntegerEnumTypeTestTrait $this
+         */
+        $this->assertTrue($enumType::addSubtype(TestSubtype::class, $regexp = '~456~'));
+        /** @var AbstractPlatform $abstractPlatform */
+        $abstractPlatform = \Mockery::mock(AbstractPlatform::class);
+        $nonMatchingValueToConvert = 99999999;
+        $this->assertNotRegExp($regexp, "$nonMatchingValueToConvert");
+        /**
+         * Used TestSubtype returns as an "enum" the given value, which is $valueToConvert in this case,
+         * @see \Doctrineum\Tests\Scalar\TestSubtype::getEnum
+         */
+        $enum = $enumType->convertToPHPValue($nonMatchingValueToConvert, $abstractPlatform);
+        $this->assertNotSame($nonMatchingValueToConvert, $enum);
+        $this->assertInstanceOf(EnumInterface::class, $enum);
+        $this->assertSame("$nonMatchingValueToConvert", (string)$enum);
+    }
+
+    /**
+     * @param EnumType $enumType
+     *
+     * @test
+     * @depends instance_can_be_obtained
+     * @expectedException \LogicException
+     * @expectedExceptionMessage Subtype of class 'Doctrineum\\Tests\\Integer\\TestSubtype' is already registered
+     */
+    public function registering_same_subtype_again_throws_exception(EnumType $enumType)
+    {
+        /** @var \PHPUnit_Framework_TestCase|IntegerEnumTypeTestTrait $this */
+        $this->assertFalse($enumType::hasSubtype(TestSubtype::class));
+        $this->assertTrue($enumType::addSubtype(TestSubtype::class, '~foo~'));
+        // registering twice - should thrown an exception
+        $enumType::addSubtype(TestSubtype::class, '~foo~');
+    }
+
+    /**
+     * @param EnumType $enumType
+     *
+     * @test
+     * @depends instance_can_be_obtained
+     * @expectedException \LogicException
+     * @expectedExceptionMessage Subtype class 'NonExistingClassName' has not been found
+     */
+    public function registering_non_existing_subtype_class_throws_exception(EnumType $enumType)
+    {
+        /** @var \PHPUnit_Framework_TestCase|IntegerEnumTypeTestTrait $this */
+        $enumType::addSubtype('NonExistingClassName', '~foo~');
+    }
+
+    /**
+     * @param EnumType $enumType
+     *
+     * @test
+     * @depends instance_can_be_obtained
+     * @expectedException \LogicException
+     * @expectedExceptionMessage Subtype class 'stdClass' lacks required method "getEnum"
+     */
+    public function registering_subtype_class_without_proper_method_throws_exception(EnumType $enumType)
+    {
+        /** @var \PHPUnit_Framework_TestCase|IntegerEnumTypeTestTrait $this */
+        $enumType::addSubtype(\stdClass::class, '~foo~');
+    }
+
+    /**
+     * @param EnumType $enumType
+     *
+     * @test
+     * @depends instance_can_be_obtained
+     * @expectedException \LogicException
+     * @expectedExceptionMessage The given regexp is not enclosed by same delimiters and therefore is not valid: 'foo~'
+     */
+    public function registering_subtype_with_invalid_regexp_throws_exception(EnumType $enumType)
+    {
+        /** @var \PHPUnit_Framework_TestCase|IntegerEnumTypeTestTrait $this */
+        $enumType::addSubtype(TestSubtype::class, /* missing opening delimiter */
+            'foo~');
+    }
+
+}
+
+/** inner */
+class TestSubtype
+{
+    public static function getEnum($value)
+    {
+        return $value;
+    }
 }
