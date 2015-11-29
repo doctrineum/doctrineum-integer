73114edc568dc83dea3ad27ad367854ba1edd352
jaroslavtyc <mail@jaroslavtyc.com>
2015-11-29 19:15:20 +0100

    Integer enum interface can be used as scalar enum interface

--- Doctrineum/Integer/IntegerEnumInterface.php
+++ Doctrineum/Integer/IntegerEnumInterface.php
@@ -1,9 +1,10 @@
 <?php
 namespace Doctrineum\Integer;
 
+use Doctrineum\Scalar\EnumInterface;
 use Granam\Integer\IntegerInterface;
 
-interface IntegerEnumInterface extends IntegerInterface
+interface IntegerEnumInterface extends IntegerInterface, EnumInterface
 {
 
 }
